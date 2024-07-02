3024db520eedd00b1794c14f3ca885e257a76449
Imad1assouli <assouli32@gmail.com>
2024-07-02 14:58:14 +0100

    first commit

---
+++ src/main/java/com/example/atlasvotegate/AtlasVoteGateApplication.java
@@ -0,0 +1,13 @@
+package com.example.atlasvotegate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasVoteGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtlasVoteGateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/atlasvotegate/AtlasVoteGateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atlasvotegate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlasVoteGateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
